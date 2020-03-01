e04d82fc81680079c4f697496857b4e6c02b3359
Jack Seymour <stilljack@gmail.com>
2020-03-01 14:04:59 -0800

    ok so template and client ex now: grab an exterior api as json (could just as easily be pointing at our servers or whatever), then processes that into the html in the template... not really any closer to creating an inline frame that reloads after a user submits some kind of instruction, but at least we have an example of going to get json and using it within a a template

--- adventure/clientex.py
+++ adventure/clientex.py
@@ -1,9 +1,19 @@
 from django import forms
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
-
+import requests
 
 def get_name(request):
+    response = requests.get('http://api.ipstack.com/104.32.252.209?access_key=88579c7866daa3c8c5407d91c627bdd5&format=1')
+    print(response)
+    geodata = response.json()
+    for item in geodata:
+        print(item)
+    if "type" in geodata:
+        print(f"yus and")
+        print(geodata["type"])
+    print(geodata)
+
     # if this is a POST request we need to process the form data
     if request.method == 'POST':
         # create a form instance and populate it with data from the request:
@@ -14,13 +24,22 @@ def get_name(request):
             # ...
             # redirect to a new URL:
             print(form.your_name)
-            return HttpResponseRedirect(f'/{form.your_name}/')
+            return render(request, 'name.html', {
+                'ip': geodata['ip'],
+                'country': geodata['country_name']
+            })
 
     # if a GET (or any other method) we'll create a blank form
     else:
         form = NameForm()
     some_value ="<h1>f templates</h1>"
-    return render(request, 'name.html', {'form': form,'some_value':some_value})
+
+    final_context ={'form': form,'some_value':some_value}
+    for k in geodata:
+        final_context[k] =geodata[k]
+
+    print(f" final context is {final_context}")
+    return render(request, 'name.html', final_context)
 
 class NameForm(forms.Form):
     your_name = forms.CharField(label='Your name', max_length=100)
