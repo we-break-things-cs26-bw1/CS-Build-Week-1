10d79bd115883b249c17d45b8f9cce1dacfbf12f
Jack Seymour <stilljack@gmail.com>
2020-03-03 10:05:30 -0800

    heroku attempt

--- api/urls.py
+++ api/urls.py
@@ -6,6 +6,7 @@ from api.api import initPlayer, playerInteract, playerMove
 urlpatterns = [
     path('', include('rest_auth.urls')),
     path('registration/', include('rest_auth.registration.urls')),
+
     url('initPlayer', initPlayer),
     url('player_interact', playerInteract),
     url('player_move', playerMove),
