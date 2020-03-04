71a140fd60cac03d4b292aa6c6857678ba588db5
patrick <patrickchow0803@gmail.com>
2020-03-04 09:29:36 -0500

    Added Update room and delete room

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import include, path
 from django.conf.urls import url
-from .views import room_list
+from .views import room_list, room_detail
 
 urlpatterns = [
     path('', include('rest_auth.urls')),
     path('registration/', include('rest_auth.registration.urls')),
     path('room/', room_list),
+    path('room/<int:pk>/', room_detail),
 ]
--- api/views.py
+++ api/views.py
@@ -20,3 +20,26 @@ def room_list(request):
             serializer.save()
             return JsonResponse(serializer.data, status=201)
         return JsonResponse(serializer.errors, status=400)
+
+@csrf_exempt
+def room_detail(request, pk):
+    try:
+        room = Room.objects.get(pk=pk)
+    except Room.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = RoomSerializer(room)
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = RoomSerializer(room, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        room.delete()
+        return HttpResponse(status=204)
