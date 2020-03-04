0d4d47d3e3367f678a7f02b9312a643c38444c00
patrick <patrickchow0803@gmail.com>
2020-03-04 09:56:06 -0500

    Made it so that room IDs show in Admin View

--- adventure/admin.py
+++ adventure/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 from .models import Room
 # Register your models here.
 
-admin.site.register(Room)
+class RoomAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'description', 'n_to', 's_to', 'e_to', 'w_to']
+
+admin.site.register(Room, RoomAdmin)
