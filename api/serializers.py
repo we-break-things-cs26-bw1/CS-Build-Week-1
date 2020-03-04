55db03080d505d16efd54081ef6ee230711a90cb
patrick <patrickchow0803@gmail.com>
2020-03-04 09:16:50 -0500

    Added a POST endpoint for creating Rooms

--- api/serializers.py
+++ api/serializers.py
@@ -4,4 +4,4 @@ from adventure.models import Room
 class RoomSerializer(serializers.ModelSerializer):
     class Meta:
         model = Room
-        fields = ['title', 'description']
+        fields = ['title', 'description', 'n_to', 's_to', 'e_to', 'w_to']
--- api/views.py
+++ api/views.py
@@ -3,7 +3,9 @@ from django.http import HttpResponse, JsonResponse
 from rest_framework.parsers import JSONParser
 from adventure.models import Room
 from .serializers import RoomSerializer
+from django.views.decorators.csrf import csrf_exempt
 
+@csrf_exempt
 def room_list(request):
     if request.method == 'GET':
         rooms = Room.objects.all()
